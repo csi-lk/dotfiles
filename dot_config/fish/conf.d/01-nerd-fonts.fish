b8555597bd0790c4464d117c69108a57d8cffb90
Callum Silcock <code@csi.lk>
2025-07-01 15:21:06 +1000

    fix:  fix startup time for fish shell
    was hanging checking for font installation

--- dot_config/fish/conf.d/01-nerd-fonts.fish
+++ dot_config/fish/conf.d/01-nerd-fonts.fish
@@ -1,24 +1,42 @@
-# Nerd Fonts detection and icon helper
-# OPTIMIZED VERSION - skips slow system_profiler check
+# Nerd Fonts detection - FAST VERSION
+# Skip slow system_profiler on macOS and use simple detection
 
-# Fast check - assume nerd fonts are available on macOS with our setup
+# On macOS, assume nerd fonts are installed
+# On Linux, do a quick check
 if test (uname) = "Darwin"
     set -gx NERD_FONT_AVAILABLE 1
 else
-    # On Linux, do a quick check for our known font
-    if fc-list 2>/dev/null | grep -q "FiraCode.*Nerd" 2>/dev/null
+    # Quick check for common nerd fonts
+    if command -q fc-list; and fc-list 2>/dev/null | grep -q "Nerd Font"
         set -gx NERD_FONT_AVAILABLE 1
     else
         set -gx NERD_FONT_AVAILABLE 0
     end
 end
 
-# Icon helper function
-function nf_icon -d "Display a Nerd Font icon with fallback"
-    if test "$NERD_FONT_AVAILABLE" = "1"
-        echo -n $argv[1]
-    else if test (count $argv) -ge 2
-        echo -n $argv[2]
+# Set environment variables for compatibility
+if test "$NERD_FONT_AVAILABLE" = "1"
+    set -gx NERD_FONT 1
+    set -gx NERD_FONT_AVAILABLE true
+else
+    set -gx NERD_FONT 0
+    set -gx NERD_FONT_AVAILABLE false
+    
+    # In Codespaces, we might not have Nerd Fonts but we can still use Unicode
+    if set -q CODESPACES
+        set -gx USE_UNICODE_ICONS true
+    end
+end
+
+# Function to get appropriate icon based on Nerd Font availability
+function nf_icon --description "Get icon with fallback for non-Nerd Font environments"
+    set -l nerd_icon $argv[1]
+    set -l fallback $argv[2]
+    
+    if test "$NERD_FONT_AVAILABLE" = "true"
+        echo -n $nerd_icon
+    else if test -n "$fallback"
+        echo -n $fallback
     else
         echo -n ""
     end
